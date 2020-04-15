4a0abeac5c7284771eb962a6e60670aa21eb2735
rahul799 <rahulagrawal799110@gmail.com>
2020-04-15 14:55:14 +0530

    transition added

--- animation.js
+++ animation.js
@@ -15,24 +15,44 @@ function animate() {
   var temp = arr1.permuteUnique();
 
   for (let i = 0; i < temp.length; i++) {
-    text.innerHTML += "<div class='random'>";
+    text.innerHTML += "<div class='div'>";
     for (let j = 0; j < temp[i].length; j++) {
-      text.innerHTML += "<span>" + temp[i][j] + "</span>";
+      text.innerHTML += "<span class='span1'>" + temp[i][j] + " , " + "</span>";
     }
     text.innerHTML += "<br>" + "</div>";
   }
 
+  let setTime = 100;
+
   let char = 0;
-  let timer = setInterval(onTick, 50);
+  let timer = setInterval(onTick, setTime * temp[0].length);
+  let childTimer = setInterval(childOnTick, setTime);
 
   function onTick() {
-    const div = text.querySelectorAll(".random")[char];
-    div.classList.add("fade");
+    const div = text.querySelectorAll(".div")[char];
+    div.classList.add("fade1");
     char++;
     if (char === temp.length) {
       complete();
+      char = 0;
       return;
     }
+  }
+
+  let count = 0;
+
+  function childOnTick() {
+    const span = text.querySelectorAll(".span1")[count];
+    span.classList.add("childFade");
+    count++;
+    if (count == temp[0].length * temp.length) {
+      childComplete();
+      return;
+    }
+  }
+
+  function childComplete() {
+    clearInterval(childTimer);
   }
 
   function complete() {
